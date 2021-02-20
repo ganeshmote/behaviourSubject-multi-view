0d855bdfe138c2b8a93551e5209e6f5752f603a3
swaraj <moteganesh10@gmail.com>
2021-02-20 20:05:14 +0530

    machinlearning app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,78 @@
-import { Component } from '@angular/core';
-
+import { Component, HostListener, ViewChild } from '@angular/core';
+import { FormArray, FormControl, FormGroup, NgForm, Validators } from '@angular/forms';
+import { Observable } from 'rxjs';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'material-demo';
+  item:any;
+  // ans1=[{info:"",ans:[]}];
+  info:any;
+  index:any;
+
+  suggested = []
+
+  other_options = ['banana', 'strawberry', 'mango', 'apple', 'pears', 'grapes']
+  myReactiveForm: FormGroup;
+  
+
+  answer = [
+    {
+      group: 'Suggested',
+      
+      items:this.suggested,
+      // items:[{infos:"",ans1:[]}],
+      
+    },
+    {
+      group: 'other options',
+      items: this.other_options,
+    },
+  ]
+
+  constructor() { }
+  ngOnInit() {
+    this.myReactiveForm = new FormGroup({
+
+      'ans': new FormControl(null),
+      'info': new FormControl(null),
+
+    });
+  }
+
+  onSubmit() {
+    console.log(this.myReactiveForm);
+    this.item = this.myReactiveForm.get('ans').value
+    this.info = this.myReactiveForm.get('info').value
+
+    if (this.suggested.indexOf(this.item) == -1) {
+      this.suggested.push(this.item)
+      // this.answer[1].items=this.suggested.slice(-2,).reverse()
+
+    } else {
+      this.index = this.suggested.indexOf(this.item)
+      this.suggested.splice(this.index, 1)
+      this.suggested.push(this.item)
+      // this.answer[1].items=this.suggested.slice(-2,).reverse()
+
+    }
+
+    this.answer = [
+      {
+        group: 'Suggested',
+        items: this.suggested.slice(-2,).reverse(),
+      },
+      {
+        group: 'other options',
+        items: this.other_options,
+      },
+    ];
+    // this.ans1.push({info:this.myReactiveForm.get('info').value,ans:[ this.myReactiveForm.get('info').value]})
+
+
+  }
 }
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { ReactiveFormsModule } from '@angular/forms'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ReactiveFormComponent } from './reactive-form/reactive-form.component';
+import { TemplateDrivenComponent } from './template-driven/template-driven.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+  
+  
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    NgbModule,
+    FormsModule
+    // MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
