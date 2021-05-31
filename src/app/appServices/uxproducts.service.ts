494528c0560362187dea8f9e5c0a821e6157020d
ganesh <ganeshm@iprogrammer.com>
2021-05-31 16:02:16 +0530

    uxtrend service

--- src/app/appServices/uxproducts.service.ts
+++ src/app/appServices/uxproducts.service.ts
@@ -1,9 +1,28 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UxproductsService {
+  url = 'https://manageproducts-bedff-default-rtdb.firebaseio.com/products.json'
+  private headers= new HttpHeaders({
+    'Content-Type':'application/json'
+  })
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
+
+  saveProducts(product: any[]) {
+    // return this.http.post(this.url,product)
+    return this.http.put(this.url, product,{headers:this.headers})
+  }
+  getProducts() {
+    // return this.http.post(this.url,product)
+    return this.http.get(this.url)
+  }
+
+  getDatatitle() {
+    // return this.http.post(this.url,product)
+    return this.http.get('https://manageproducts-bedff-default-rtdb.firebaseio.com/dataTitle.json')
+  }
 }
